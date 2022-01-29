1f36040e260743c72d391bc22f2a29a352c4439b
Evgeniya Bayda <baje75128@gmail.com>
2022-01-30 01:14:03 +0200

    add styles

--- js/scroll.js
+++ js/scroll.js
@@ -3,6 +3,7 @@ anchors.forEach((anchor) => {
   anchor.addEventListener("click", (e) => {
     e.preventDefault();
     const elementId = anchor.getAttribute("href").substr(1);
+    console.log(elementId);
     document.getElementById(elementId).scrollIntoView({
       behavior: "smooth",
       block: "start",
