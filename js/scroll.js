cefe2462c4044ae634018f7d5ebefbfdce4eea94
Evgeniya Bayda <baje75128@gmail.com>
2022-01-30 10:13:23 +0200

    update styles

--- js/scroll.js
+++ js/scroll.js
@@ -1,9 +1,9 @@
-const anchors = document.querySelectorAll('a[href*="#"]');
+const anchors = document.querySelectorAll('a[href^="#"]');
+
 anchors.forEach((anchor) => {
   anchor.addEventListener("click", (e) => {
     e.preventDefault();
     const elementId = anchor.getAttribute("href").substr(1);
-    console.log(elementId);
     document.getElementById(elementId).scrollIntoView({
       behavior: "smooth",
       block: "start",
